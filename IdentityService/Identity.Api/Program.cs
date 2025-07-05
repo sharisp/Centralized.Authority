877b2aaa24b68abb1347700a7b8febef8e4f2ece
Andrew Wang <wthsir@gmail.com>
2025-07-05 21:02:32 +1000

    refactor:use my jwt nuget instead of jwt library

--- IdentityService/Identity.Api/Program.cs
+++ IdentityService/Identity.Api/Program.cs
@@ -22,14 +22,7 @@ namespace Identity.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
-            // builder.Services.AddControllers();
-
-
-
-
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+         
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddJWTAuthentication(builder.Configuration);
