4a00006374da871f9453c0d40926daa76ecf6003
Andrew Wang <wthsir@gmail.com>
2025-08-21 12:30:17 +1000

    refactor: add comment

--- IdentityService/Identity.Api/ValidationHelper.cs
+++ IdentityService/Identity.Api/ValidationHelper.cs
@@ -5,6 +5,14 @@ namespace Identity.Api
 
     public class ValidationHelper
     {
+        /// <summary>
+        /// validate model with FluentValidation
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="model"></param>
+        /// <param name="validator"></param>
+        /// <returns></returns>
+        /// <exception cref="ValidationException"></exception>
         public static async Task ValidateModelAsync<T>(T model, IValidator<T> validator)
         {
             var result = await validator.ValidateAsync(model);
