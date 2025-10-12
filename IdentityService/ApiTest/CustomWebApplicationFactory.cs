5b672a4c4798a813b7aece43079e447f63aab1bb
Andrew Wang <wthsir@gmail.com>
2025-10-12 20:31:38 +1100

    test: add inmemory db

---
+++ IdentityService/ApiTest/CustomWebApplicationFactory.cs
@@ -0,0 +1,37 @@
+﻿using Domain.SharedKernel.Interfaces;
+using Identity.Api;
+using Identity.Infrastructure;
+using Identity.Infrastructure.Repository;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using Xunit;
+namespace ApiTest;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("XunitTesting");
+
+        builder.ConfigureServices(services =>
+        {
+            services.AddDbContext<BaseDbContext>(options =>
+                options.UseInMemoryDatabase("TestDb"));
+        });
+
+        builder.Configure(app =>
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<BaseDbContext>();
+            db.Database.EnsureCreated();
+            db.Users.Add(new Identity.Domain.Entity.User("testuser", "11@11.com", "123456"));
+            db.SaveChanges();
+        });
+    }
+}
+
+
+
