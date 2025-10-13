6b90a54a849012a3e1fddd0468fb0fbdd115e455
Andrew Wang <wthsir@gmail.com>
2025-10-13 16:42:29 +1100

    test: role repository add

---
+++ IdentityService/InfraTest/RoleRepositoryTest.cs
@@ -0,0 +1,34 @@
+﻿using Identity.Domain.Entity;
+using Identity.Domain.Enums;
+using Identity.Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraTest
+{
+    [Collection("Global Test Collection")]
+    public class RoleRepositoryTest
+    {
+        BaseDbContext context;
+        public RoleRepositoryTest(DBContextFixture dBContextFixture)
+        {
+            context = dBContextFixture.baseDbContext;
+        }
+        [Fact]
+        public void Test1()
+        {
+
+            var role = new Role("TestRole");
+            context.Roles.Add(role);
+            context.SaveChanges();
+
+            var repo = new RoleRepository(context);
+            var model = repo.Query().FirstOrDefault();
+            Assert.NotNull(model);
+
+        }
+    }
+}
