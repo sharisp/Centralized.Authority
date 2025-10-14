da8d7bcba79b2fc39df49af2505557399bb6a69f
Andrew Wang <wthsir@gmail.com>
2025-10-14 11:56:45 +1100

    test: role repository query with permission

--- IdentityService/InfraTest/RoleRepositoryTest.cs
+++ IdentityService/InfraTest/RoleRepositoryTest.cs
@@ -30,5 +30,51 @@ namespace InfraTest
             Assert.NotNull(model);
 
         }
+
+        [Fact]
+        public void TestWithMenu()
+        {
+
+            var role = new Role("TestRole");
+            role.AddMenus(new List<Menu> {
+            new Menu("Menu1", "Menu1", 0, 1, MenuType.Menu, "TES", null),
+            new Menu("Menu2", "Menu2", 0, 1, MenuType.Menu, "TES", null)
+
+            });
+            context.Roles.Add(role);
+
+            context.SaveChanges();
+
+            var repo = new RoleRepository(context);
+            var model = repo.Query().FirstOrDefault();
+            Assert.NotNull(model);
+
+            var modelNew = repo.Query(false,true).FirstOrDefault();
+            Assert.NotNull(modelNew?.Menus);
+
+        }
+
+        [Fact]
+        public void TestWithPermissions()
+        {
+
+            var role = new Role("TestRole");
+            role.AddPermissions(new List<Permission> {
+         new Permission("Perm1", "Perm1", "Test"),
+         new Permission("Perm2", "Perm2", "Test"),
+
+            });
+            context.Roles.Add(role);
+
+            context.SaveChanges();
+
+            var repo = new RoleRepository(context);
+            var model = repo.Query().FirstOrDefault();
+            Assert.NotNull(model);
+
+            var modelNew = repo.Query(true, false).FirstOrDefault();
+            Assert.NotNull(modelNew?.Permissions);
+
+        }
     }
 }
