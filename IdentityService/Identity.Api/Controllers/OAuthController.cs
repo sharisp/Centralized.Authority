a19fc4de82f12993bfa2622a47c00da8790bcf92
Andrew Wang <wthsir@gmail.com>
2025-10-31 15:38:02 +1100

    feat: oauth controller

---
+++ IdentityService/Identity.Api/Controllers/OAuthController.cs
@@ -0,0 +1,19 @@
+﻿using Identity.Api.Contracts.Dtos.Response;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [AllowAnonymous]
+    [ApiController]
+    public class OAuthController : ControllerBase
+    {
+        [HttpGet("google")]
+        public  ActionResult<ApiResponse<string>> GoogleCallBack(string code,string state)
+        {
+            return this.OkResponse(code);
+        }
+    }
+}
