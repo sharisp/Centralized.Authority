5ee56a923bc5d1e81f0a9bc426e350051e9181b9
Andrew Wang <wthsir@gmail.com>
2025-11-04 22:28:40 +1100

    feat: add ISystemConfigRepository

---
+++ IdentityService/Identity.Domain/Interfaces/ISystemConfigRepository.cs
@@ -0,0 +1,15 @@
+﻿using Identity.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Domain.Interfaces
+{
+    public interface ISystemConfigRepository
+    {
+        Task<List<SystemConfig>> GetAllActive();
+        Task<SystemConfig?> GetByConfigKey(string configKey,string? systemName);
+    }
+}
