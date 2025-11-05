20b9b60487c54704aee666feb79ac2fe122b2fc1
Andrew Wang <wthsir@gmail.com>
2025-11-05 22:29:38 +1100

    feat: add systemconfig repository

---
+++ IdentityService/Identity.Infrastructure/Repository/SystemConfigRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Infrastructure.Repository
+{
+    public class SystemConfigRepository : ISystemConfigRepository
+    {
+        public Task<List<SystemConfig>> GetAllActive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SystemConfig?> GetByConfigKey(string configKey, string? systemName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
